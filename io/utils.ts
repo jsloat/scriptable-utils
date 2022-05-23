60bea5471c99762e74b64dfe9cb49fd28bffbd2e
John Sloat <jsloat1@gmail.com>
2022-05-22 20:15:41 -0700

    Add temp file cleanup util

--- io/utils.ts
+++ io/utils.ts
@@ -1,5 +1,27 @@
+import { SCRIPTABLE_STORE_PATH } from './consts';
+import { getDirContents } from './filesystemUtils';
+import persisted from './persisted';
+
 const temporaryFilenamePrefix = 'tmp_';
 
 /** Generates a filename indicative of a temporary file that can be cleaned up. */
 export const getTemporaryFilename = () =>
   `${temporaryFilenamePrefix}${UUID.string()}`;
+
+/** Deletes all temporary files that exist in the Scriptable `store` directory
+ * - this is a custom directory that I use for persisted data files. These
+ *   temporary files are used by things like `PersistedCache` to temporarily
+ *   store data.  */
+export const cleanupTemporaryFiles = () => {
+  const allStoreFiles = getDirContents(SCRIPTABLE_STORE_PATH);
+  const tempFilenames = allStoreFiles
+    .map(f => f.filenameNoExtension)
+    .filter(filename => filename.startsWith(temporaryFilenamePrefix));
+  tempFilenames.forEach(async filename => {
+    const io = persisted<any>({ filename, defaultData: null });
+    // Ensure file is downloaded first, otherwise may not be deleted.
+    await io.getData();
+    io.deleteFile();
+  });
+  return { numDeletedTempFiles: tempFilenames.length };
+};
