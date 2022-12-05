500ef55f687fba601a534b975f1d6f7bd34e442a
John Sloat <jsloat1@gmail.com>
2022-12-05 00:42:04 -0800

    Start migration of old table els

--- UITable/Row/templates/_H1.ts
+++ UITable/Row/templates/_H1.ts
@@ -1,11 +1,7 @@
 import { conditionalArr } from '../../../array';
-import { getColor } from '../../../colors';
 import { SFSymbolKey } from '../../../sfSymbols';
+import { H1 } from '../../elements';
 import { RowOpts } from '../types';
-import { H1Consts } from './consts';
-import { composeRowConstructor } from './utils';
-import { getFootnoteReducer } from './_Footnote';
-import _ThreeCol, { getThreeColReducer } from './_ThreeCol';
 
 export type H1Opts = {
   title: string;
@@ -17,68 +13,11 @@ export type H1Opts = {
   icon?: SFSymbolKey;
 } & Pick<RowOpts, 'onTap' | 'isFaded'>;
 
-//
-
-const TopRow = ({
-  icon,
-  title,
-  titleColor,
-  onTap,
-  badgeNumber,
-  subtitle,
-  isFaded,
-}: H1Opts) => {
-  const { textSize, fontConstructor, paddingTop, paddingBottom } = H1Consts;
-  return _ThreeCol({
-    ...(icon && { gutterLeft: { iconKey: icon } }),
-    main: {
-      text: title,
-      fontConstructor,
-      textSize,
-      ...(titleColor && { color: titleColor }),
-    },
-    ...(badgeNumber && {
-      gutterRight: {
-        text: badgeNumber,
-        color: getColor('danger'),
-      },
-    }),
-    onTap,
-    isFaded,
-    padding: { paddingTop, paddingBottom: subtitle ? 0 : paddingBottom },
-    rowHeight: 'lg',
+export default ({ title, badgeNumber, subtitle, ...rest }: H1Opts) =>
+  H1(title, {
+    subtitle:
+      subtitle || badgeNumber
+        ? conditionalArr([subtitle, badgeNumber]).join(', ')
+        : undefined,
+    ...rest,
   });
-};
-
-const BottomRow = ({
-  subtitle,
-  icon,
-  subtitleColor,
-  badgeNumber,
-  onTap,
-  isFaded,
-}: H1Opts) => {
-  if (!subtitle) return null;
-  const { paddingBottom } = H1Consts;
-  const rowConstructor = composeRowConstructor(
-    getThreeColReducer({
-      ...(icon && { gutterLeft: { isEmpty: true } }),
-      main: {
-        text: subtitle,
-        ...(subtitleColor && { color: subtitleColor }),
-      },
-      ...(badgeNumber && { gutterRight: { isEmpty: true } }),
-    }),
-    getFootnoteReducer()
-  );
-  return rowConstructor({
-    onTap,
-    padding: { paddingTop: 0, paddingBottom },
-    isFaded,
-  });
-};
-
-//
-
-export default (opts: H1Opts) =>
-  conditionalArr([TopRow(opts), BottomRow(opts)]).flat();
