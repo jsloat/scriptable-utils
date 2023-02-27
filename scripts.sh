7d7a637df8d85d34aea6369b1c035dfc5b947da6
John Sloat <jsloat1@gmail.com>
2023-02-27 09:13:23 -0800

    Add cyclic dependency check

--- scripts.sh
+++ scripts.sh
@@ -4,8 +4,26 @@ lint() {
   eslint "./src/**/*.{ts,js}" --fix
 }
 
+circDeps() {
+  madge ./src/index.ts --circular
+}
+
 tests() {
   clear
-  if ! lint; then return; fi
-  tsc
+  echo "Linting..."
+  if ! lint; then
+    echo "Lint failed"
+    return 1
+  fi
+  echo "Running tsc..."
+  if ! tsc; then
+    echo "tsc failed"
+    return 1
+  fi
+  echo "Checking for circular deps..."
+  if ! circDeps; then
+    echo "Circular dependencies failed"
+    return 1
+  fi
+  return 0
 }
